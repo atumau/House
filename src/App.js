0fe03ba91397b49d3a8f5ab1023783d047d97822
atul <mauryaatul328@gmail.com>
2023-09-18 17:59:01 +0530

    All Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './Components/header';
+import Footer from './Components/footer';
+import Home from './Components/home';
+import FeaturedHome from './Components/featuredhome';
+import About from './Components/about';
+import Homepage from './Components/homepage';
+import Agents from './Components/agents';
+import Contact from './Components/contact';
+import List from './Components/list';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+      <Route path="/" element={<Homepage />} />
+        <Route path="/header" element={<Header />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/footer" element={<Footer />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/homepage" element={<Homepage />} />
+        <Route path="/featuredhome" element={<FeaturedHome />} />
+        <Route path="/agents" element={<Agents/>}/>
+        <Route path="/contact" element={<Contact/>}/>
+        <Route path="/list" element={<List/>}/>
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
